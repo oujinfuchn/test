0c8a6f17d2a9e11753ec197888f08b56c156d940
oujinfu <oujinfuchn@163.com>
2017-07-07 16:42:41 +0800

    初始化项目

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Edison on 17/7/7.
+//  Copyright © 2017年 Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Edison on 17/7/7.
+//  Copyright © 2017年 Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
